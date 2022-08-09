74a046bb5ce9bc7b6c6f3361adfb64561af1c17a
Yusuf Tunc <83668748+yusuftunc1@users.noreply.github.com>
2022-08-09 08:11:45 +0300

    Add files via upload

---
+++ wpbot/wpbot.py
@@ -0,0 +1,47 @@
+import pyautogui as pg
+import webbrowser as web
+import time
+import pandas as pd
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+
+df1 = pd.read_excel('04.08sms.xlsx')
+print(df1)
+
+
+first = True
+for i in range(len(df1['Phone'])):
+    row = df1.loc[i]
+    name = str(row[1]).lower()
+    username = str(row[2]).lower()
+    birleşik = name + username
+    password = birleşik.replace(' ', '')
+
+    message = f"Mobil Bayiliğiniz aktif olmuştur. Deterjanpazari.com adresinden hesabınıza giriş yaparak kontrolleri sağlayınız.\
+Hesap Bilgileriniz;         \
+        Mail: {row[0]}      \
+        Şifre: {password}      \
+Referans kodunuz, hesabım sayfasının alt kısmında bulunmaktadır. Bu kod ile getireceğiniz her üye sizin müşteriniz olarak hesabınıza tanımlanacaktır. İYİ GÜNLER DİLERİZ. deterjanpazari.com"
+    time.sleep(4)
+    try:
+        web.open("https://web.whatsapp.com/send?phone=+90"+str(row[3])+"&text="+message)
+    except: 
+        ws[f"A{i+1}"] = row[0]
+        ws[f"B{i+1}"] = row[1]
+        ws[f"C{i+1}"] = row[2]
+        ws[f"D{i+1}"] = row[3]
+    if first:
+        time.sleep(6)
+        first=False
+    width,height = pg.size()
+    pg.click(width/2,height/2)
+    time.sleep(5)
+    pg.press('enter')
+    time.sleep(5)
+    pg.hotkey('ctrl', 'w')
+
+
+wb.save(f"mesaj gönderimi sırasında hata olanlar.xlsx")
+
